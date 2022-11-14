027fc4598a3c4b6bba8c4ac21f518b8b224d3c63
Kikh31 <artem.kikhtenko33@gmail.com>
2022-11-14 21:36:21 +0300

    commit

---
+++ lab4/C.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct priorityQueue {
+    vector<pair<int, int>> vec;
+    int oper = 0;
+
+    void siftDown(int n, int i) {
+        while (2 * i + 1 < n) {
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+            int min = l;
+            if (r < n && vec[r].first < vec[min].first) min = r;
+
+            if (vec[i].first <= vec[min].first) break;
+            swap(vec[i], vec[min]);
+            i = min;
+        }
+    }
+
+    void siftUp(int i) {
+        while (vec[i].first < vec[(i - 1) / 2].first) {
+            swap(vec[i], vec[(i - 1) / 2]);
+            i = (i - 1) / 2;
+        }
+    }
+
+    void push(int val) {
+        vec.push_back({val, ++oper});
+        int n = vec.size() - 1;
+        siftUp(n);
+    }
+
+    void extract_min() {
+        oper++;
+        if(!vec.empty()) {
+            int min = vec[0].first;
+            swap(vec[0], vec[vec.size()-1]);
+            vec.pop_back();
+            siftDown(vec.size(), 0);
+            cout<<min<<endl;
+        } else {
+            cout<<'*'<<endl;
+        }
+    }
+
+    void decrease_key(int x, int y) {
+        oper++;
+        for(int i = 0; i < vec.size(); i++) {
+            if(vec[i].second == x) {
+                vec[i].first = y;
+                siftUp(i);
+            }
+        }
+    }
+};
+
+int main() {
+    priorityQueue q;
+    string cmd;
+    while (cin>>cmd) {
+        if(cmd == "push") {
+            int val;
+            cin>>val;
+            q.push(val);
+        }
+        else if(cmd == "extract-min") q.extract_min();
+        else if(cmd == "decrease-key") {
+            int x, y;
+            cin>>x>>y;
+            q.decrease_key(x,y);
+        }
+    }
+}
--- lab4/D.cpp
+++ lab4/D.cpp
@@ -1,2 +1,43 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
+using namespace std;
+
+void countingSort(string *arr, int n, int div) {
+    int k = -1;
+    for (int i = 0; i < n; i++) {
+        k = max(k, arr[i][div] + 1);
+    }
+
+    int C[k] = {0};
+    for (int i = 0; i < n; ++i) C[arr[i][div]]++;
+
+    for (int i = 1; i < k; ++i) {
+        C[i] += C[i - 1];
+    }
+
+    string resArr[n];
+    for (int i = n - 1; i >= 0; --i) {
+        resArr[C[arr[i][div]] - 1] = arr[i];
+        C[arr[i][div]]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = resArr[i];
+    }
+}
+
+void radixSort(string *arr, int n, int m, int k) {
+    for(int i = m - 1; i >= m - k; i--) {
+        countingSort(arr, n, i);
+    }
+}
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    string arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    radixSort(arr, n, m, k);
+    for(int i = 0; i < n; i++) cout<<arr[i]<<endl;
+}
